caeaf824a4cbafd918209b43a11289746c90cfd5
Amar-Nath-Singh <104088805+Amar-Nath-Singh@users.noreply.github.com>
2022-04-20 22:25:02 +0530

    Update Ques2.py

--- Ques2.py
+++ Ques2.py
@@ -1,6 +1,6 @@
 def int_input():
     return list(map(int,input().split()))
-def binary_search(arr, x):
+def binary_search(arr, x): #Algo to search a number in 1-D Sorted array
     low = 0
     high = len(arr) - 1
     mid = 0
@@ -19,10 +19,12 @@ def binary_search(arr, x):
 [k] = int_input()
 arr = []
 for _ in range(m):
+    #Converting Matrix to List of Sorted Elements
     for x in list(map(int,input().split())):
         arr.append(x)
-ind = binary_search(arr,k)+1
+ind = binary_search(arr,k)+1 # Position to value in list
 if ind !=0:
+    #Converting Postion in List to Position in Matrix
     i=int(ind/n)
     j = ind - n*i -1
     print("True")
